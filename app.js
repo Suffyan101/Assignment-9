6775cea4a87091da3a74e22f8ad40df270ea6a42
Suffyan101 <suffyan.r01@gmail.com>
2022-07-04 22:30:43 +0500

    Assignment 9

---
+++ app.js
@@ -0,0 +1,41 @@
+// Question 1
+// document.getElementById("Q1").innerHTML = new Date()
+
+// Question 2
+// var a = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+// var b =  new Date().getMonth();
+// console.log(b)
+// var c = a[b]
+// alert(c)
+
+// Question 3
+// var b = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+// var a  = new Date().getDay()
+// alert(b[a])
+
+
+// Question 4
+
+// var a = new Date().getDay()
+
+// if(a === 0 || a === 6){
+//     document.write("Its Fun Day..!")
+// }
+// else{
+//     document.write("Working Day")
+// }
+
+// Question 5
+
+// var a = new Date().getDate();
+
+// if(a<=15){
+//     document.write("First Fifteen days of the month")
+// }
+// else{
+//     document.write(a)
+// }
+
+
+// Question 6
+
