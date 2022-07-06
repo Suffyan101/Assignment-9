7223c0473e9ad387de7fefd046cbd6bb927d51d8
Suffyan101 <suffyan.r01@gmail.com>
2022-07-07 03:00:04 +0500

    update

--- app.js
+++ app.js
@@ -61,3 +61,72 @@
 // var laterDate = new Date("12 31 2022")
 // console.log("later Date: "+laterDate)
 
+// Question 9
+
+// var a = new Date("06 18 2015").getTime();
+
+// var b = new Date().getTime();
+
+// var c = b - a;
+
+// var d = c/1000/60/60/24
+// var e = d.toFixed()
+// document.write(e+" days have passed since 1st Ramadan, 2015")
+
+
+// Question 10
+
+// var a = new Date("01 01 2015")
+// document.write("On refrence date "+ a+ "<br>")
+// var b = a.getTime()
+// var c = new Date().getTime()
+// var d = c - b
+// var e = d.toFixed()
+// document.write(d+ " Seconds had passed since begining of 2015")
+
+
+// Question 11
+
+// var a = new Date();
+// var b = new Date().getHours();
+// document.write("Current date: "+ a + "<br>")
+// document.write(b+" hour ago, it was "+ a)
+
+// // Question 12
+// var a= new Date()
+// var b = new Date("07 07 1922")
+// document.write("Current Date: "+a+"<br>")
+// document.write("100 years back, it was "+ b)
+
+// Question 13 
+// function myfunction(){
+//     var a = new Date().getTime();
+//     var b = document.getElementById("demo").value;
+//     var c = new Date(b).getTime();
+//     var d = a-c
+//     var e = d/1000/60/60/24/365
+//     var f = new Date().getFullYear()
+//     var g = f - h;
+//     var h = e.toFixed()
+//     document.getElementById("demo1").innerHTML = "Your age is "+ h
+//     document.getElementById("demo2").innerHTML ="Your birth year is "+ g
+// }
+
+// Question 14
+
+function billgenerate(){
+    
+var a = document.getElementById("demo").value;
+var b = document.getElementById("demo1").value;
+var c = document.getElementById("demo2").value;
+var d = document.getElementById("demo3").value;
+var e = 350
+var f = document.getElementById("demo7").value;
+document.getElementById("demo8").innerHTML = "Customer Name: "+a
+document.getElementById("demo9").innerHTML = "Month: "+f;
+document.getElementById("demo4").innerHTML = "Net Amount Payable (Within Due Date): "+ c*d
+document.getElementById("demo5").innerHTML = "Late payment surcharge: "+e;
+document.getElementById("demo6").innerHTML = "Gross Amount Payable (After Due Date): "+ c*d+e
+
+
+}
